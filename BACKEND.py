b23da67157e9a73e09e8196f1776e75d42c72d6b
Noa Shem Tov <noa33555@gmail.com>
2025-09-25 05:06:20 +0300

    Model Replacement

--- BACKEND.py
+++ BACKEND.py
@@ -9,7 +9,6 @@ from openai import OpenAI  # OpenAI client library to interact with OpenAI / Hug
 import google.generativeai as genai  # Google Generative AI client library for using Gemini and other Google models
 import requests  # Standard library for sending HTTP requests, useful for APIs without a dedicated client
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM #for model code5T
-from LoadCodet5 import tokenizer, model
 
 #-----------------------------------------MAIN
 app = Flask( __name__,)
